d428829d4d09e8f1ecd63cbccf9cf8740d743a2f
Jaime Fernandez <luis-jaime.fernandez@atos.net>
2022-07-13 19:06:37 -0500

    Ultimo doc de sql

---
+++ store-procedures.sql
@@ -0,0 +1,22 @@
+USE ejemplo
+GO
+	
+CREATE PROCEDURE AsignarProjecto (@comp VARCHAR(50), @project VARCHAR(50))
+AS
+BEGIN
+	DECLARE @empleado VARCHAR(50), @idEmpleado INT, @idEmpresa INT, @flag BIT
+	SELECT @idEmpresa = Id FROM Projects WHERE Name = @comp
+	DECLARE empleadosCursor CURSOR
+		FOR SELECT Id, FirstName FROM Employees
+	OPEN empleadosCursor
+	FETCH empleadosCursor INTO @idEmpleado, @empleado
+	WHILE(@@FETCH_STATUS=0)
+	BEGIN
+		SELECT @flag = dbo.HasAnyProjec(@empleado)
+		IF(@flag = 1)
+			BEGIN
+				UPDATE PivoteProjectsEmployees SET ProjectID = @idEmpresa WHERE EmployeesID = @idEmpleado
+			END
+	END
+END
+GO
